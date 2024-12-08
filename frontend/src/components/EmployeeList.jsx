5fb320ef07fb8bfcc976d0d4d728e3b2fd7632ff
ℑ𝔯𝔬𝔫_𝔐𝔞𝔯𝔱𝔦𝔫ℌ𝔢𝔞𝔯𝔱 🎮🎯👾⚡❤‍🩹 <138081463+AnthonyMartinHeart@users.noreply.github.com>
2024-12-08 14:42:42 -0300

    Update EmployeeList.jsx

--- frontend/src/components/EmployeeList.jsx
+++ frontend/src/components/EmployeeList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Swal from 'sweetalert2'; 
-import '@styles/EmployeeList.css'; 
+import '@styles/EmployeeList.css';
 
 const EmployeeList = ({ employees, userRole }) => {
     const [turnos, setTurnos] = useState({});
