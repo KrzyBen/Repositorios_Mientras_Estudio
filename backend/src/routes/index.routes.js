44b0babe715a2e79bc1fd5f241169ae9f99f1cb8
Zack__VLZ <118009633+ZackPowerVLZ@users.noreply.github.com>
2025-05-29 02:34:43 -0400

    Update index.routes.js
    Pequeña modificación para estabilidad de los nuevos elementos añadidos

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,13 +1,16 @@
-"use strict";
-import { Router } from "express";
-import userRoutes from "./user.routes.js";
-import authRoutes from "./auth.routes.js";
-import cuponRoutes from "./cuponPago.routes.js";
+'use strict';
+import { Router } from 'express';
+import userRoutes from './user.routes.js';
+import authRoutes from './auth.routes.js';
+import cuponRoutes from './cuponPago.routes.js';
+import reclamosRoutes from './reclamos.routes.js';
 
 const router = Router();
 
 router
-    .use("/auth", authRoutes)
-    .use("/user", userRoutes)
-    .use("/cupon", cuponRoutes);
+  .use('/auth', authRoutes)
+  .use('/user', userRoutes)
+  .use('/cupon', cuponRoutes)
+  .use('/reclamos', reclamosRoutes);
+
 export default router;
